3a78be1ae7994d6a59d59fcd8f5c13f379c6058a
Sasha <sasha7692@gmail.com>
2019-12-13 23:08:11 +0200

    Create install_artifactory.sh

---
+++ install_artifactory.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Install Artifactory in Ubuntu 18.04
+
+# Check Java
+java_status=$(dpkg -l | grep java)
+if [[ $java_status == "" ]]
+then
+echo "Java is not installed"
+sudo apt install -y openjdk-8-jre-headless
+else echo "Java is already installed"
+fi
+
+# Install Artifactory
+# wget https://bintray.com/artifact/download/jfrog/artifactory-debs/pool/main/j/jfrog-artifactory-oss-deb/jfrog-artifactory-oss-6.16.0.deb
+wget -c -O - "https://bintray.com/user/downloadSubjectPublicKey?username=jfrog" | sudo apt-key add -
+echo "deb https://bintray.com/artifact/download/jfrog/artifactory-debs trusty main" | sudo tee -a /etc/apt/sources.list.d/artifactory-oss.list
+sudo apt-get update
+sudo apt-get -y install jfrog-artifactory-oss
+sudo ufw allow 8081
