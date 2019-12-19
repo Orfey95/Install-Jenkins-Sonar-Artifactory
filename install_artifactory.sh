46cd7692465f02d38ae756a4f818af3e2c40d23a
Sasha <sasha7692@gmail.com>
2019-12-19 16:35:46 +0200

    Update install_artifactory.sh

--- install_artifactory.sh
+++ install_artifactory.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
-# Install Artifactory in Ubuntu 18.04
+# Install Artifactory in Centos 7
 
 # Check Java
-java_status=$(dpkg -l | grep java)
+java_status=$(sudo rpm -qa | grep java)
 if [[ $java_status == "" ]]
 then
 echo "Java is not installed"
-sudo apt install -y openjdk-8-jre-headless
+sudo yum install -y java-1.8.0-openjdk java-1.8.0-openjdk-devel
 else echo "Java is already installed"
 fi
 
+# Set JAVA_HOME
+export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))
+source ~/.bashrc
+export PATH=$PATH:$JAVA_HOME/bin
+export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar
+
+# Add Artifactory RPM repository
+curl -sL https://bintray.com/jfrog/artifactory-rpms/rpm | sudo tee /etc/yum.repos.d/bintray-jfrog-artifactory-rpms.repo
+
 # Install Artifactory
-# wget https://bintray.com/artifact/download/jfrog/artifactory-debs/pool/main/j/jfrog-artifactory-oss-deb/jfrog-artifactory-oss-6.16.0.deb
+artifactory_status=$(sudo rpm -qa | grep artifactory)
+if [[ $artifactory_status == "" ]]
+then
+echo "Artifactory is not installed"
+sudo yum -y install jfrog-artifactory-oss wget
+else echo "Java is already installed"
+fi
+sudo systemctl start artifactory.service
 
-# sudo wget -O artifactory-oss-6.16.0.tar.gz https://bintray.com/jfrog/artifactory/download_file?file_path=jfrog-artifactory-oss-6.16.0-sources.tar.gz
-# tar xvzf artifactory-oss-6.16.0.tar.gz
-# cd jfrog-artifactory-6.16.0/distribution/standalone/zip/src/main/install/bin
+# Install Artifactory CLI
+curl -fL https://getcli.jfrog.io | sh
 
-
-# CentOS
-sudo yum install -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel wget nano
-sudo wget -O artifactory-oss-6.16.0.rpm https://bintray.com/jfrog/artifactory-rpms/download_file?file_path=jfrog-artifactory-oss-6.16.0.rpm
-sudo yum install -y artifactory-oss-6.16.0.rpm
-
-
-sudo wget -c -O - "https://bintray.com/user/downloadSubjectPublicKey?username=jfrog" | sudo apt-key add -
-echo "deb https://bintray.com/artifact/download/jfrog/artifactory-debs trusty main" | sudo tee -a /etc/apt/sources.list.d/artifactory-oss.list
-sudo apt-get update
-sudo apt-get -y install jfrog-artifactory-oss
-sudo ufw allow 8081
+# Create maven repository
