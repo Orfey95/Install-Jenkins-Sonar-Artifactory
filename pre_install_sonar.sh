0ac35fe6353ec6cd2087b43a14c048f53f16f2d0
Sasha <sasha7692@gmail.com>
2020-04-09 11:21:19 +0300

    Update pre_install_sonar.sh

--- pre_install_sonar.sh
+++ pre_install_sonar.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+# PREInstall Sonar on Centos 7
+
+# Turn on logging
+set -x
+
+# Increase the limits
+echo "vm.max_map_count = 262144" >> /etc/sysctl.conf
+echo "fs.file-max = 65536" >> /etc/sysctl.conf
+echo "sonar   -   nofile   65536" >> /etc/security/limits.d/99-sonarqube.conf
+echo "sonar   -   nproc    2048" >> /etc/security/limits.d/99-sonarqube.conf
+
+
+# Reboot
+reboot
