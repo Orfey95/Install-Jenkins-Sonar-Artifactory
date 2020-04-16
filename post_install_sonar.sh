0d21292da53f66d110171503963606344751668f
Sasha <sasha7692@gmail.com>
2020-04-16 12:45:08 +0300

    Update post_install_sonar.sh

--- post_install_sonar.sh
+++ post_install_sonar.sh
@@ -8,6 +8,8 @@ set -x
 # Create user
 password=$1
 
+bash /opt/sonarqube/bin/linux-x86-64/sonar.sh start
+
 curl -u admin:admin -X POST http://localhost:9000/api/users/change_password --data 'login=admin&password=$password&previousPassword=admin'
 
 sed -i 's/ExecStart=/home/\#ExecStart=\/home/' /etc/systemd/system/sonar.service
