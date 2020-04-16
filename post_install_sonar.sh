e53237a8ad5c243cc7873a6f1480fc9e0f507dc9
Sasha <sasha7692@gmail.com>
2020-04-16 12:19:15 +0300

    Update post_install_sonar.sh

--- post_install_sonar.sh
+++ post_install_sonar.sh
@@ -6,11 +6,8 @@
 set -x
 
 # Create user
-login=$1
-password=$2
-name=$3
-email=$4
+password=$1
 
-curl -X POST -v -u admin:admin "http://localhost:9000/api/users/create?login=$login&password=$password&name=$name&email=$email"
+curl -u admin:admin -X POST http://localhost:9000/api/users/change_password --data 'login=admin&password=$password&previousPassword=admin'
 
 
