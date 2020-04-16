b37c402ef942134d12dbd9e9177554574d754cec
Sasha <sasha7692@gmail.com>
2020-04-16 12:35:09 +0300

    Update post_install_sonar.sh

--- post_install_sonar.sh
+++ post_install_sonar.sh
@@ -10,6 +10,6 @@ set -x
 
 curl -u admin:admin -X POST http://localhost:9000/api/users/change_password --data 'login=admin&password=password&previousPassword=admin'
 
-sed -i 's/ExecStart=curl/\#ExecStart=curl/' /etc/security/limits.d/99-sonarqube.conf
+sed -i 's/ExecStart=curl/\#ExecStart=curl/' /etc/systemd/system/sonar.service
 
 
