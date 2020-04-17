00f7a2d1525a052f26bcc042c84aac4731364ba7
Sasha <sasha7692@gmail.com>
2020-04-17 10:13:45 +0300

    Update post_install_sonar.sh

--- post_install_sonar.sh
+++ post_install_sonar.sh
@@ -3,13 +3,7 @@
 # POSTInstall Sonar on Centos 7
 
 # Turn on logging
-#set -x
-
-#until $(curl --output /dev/null --silent --head --fail http://localhost:9000); do
-#sleep 3
-#done
-
-#curl -u admin:admin -X POST http://localhost:9000/api/users/change_password --data 'login=admin&password=replace_password&previousPassword=admin'
+set -x
 
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' -u admin:admin -X POST http://localhost:9000/api/users/change_password --data 'login=admin&password=replace_password&previousPassword=admin')" != "204" ]]; 
 do sleep 5; 
