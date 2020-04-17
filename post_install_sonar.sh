1e4ac332010ebdc15b9a7e8d4cbc9f8160167f06
Sasha <sasha7692@gmail.com>
2020-04-17 10:55:51 +0300

    Update post_install_sonar.sh

--- post_install_sonar.sh
+++ post_install_sonar.sh
@@ -6,14 +6,20 @@
 iptables -A INPUT -p tcp -s localhost --dport 9000 -j ACCEPT
 iptables -A INPUT -p tcp --dport 9000 -j DROP
 
+echo "Firewall ON" >> /var/log/temp_sonar.log
+
 # API change admin password
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' -u admin:admin -X POST http://localhost:9000/api/users/change_password --data 'login=admin&password=replace_password&previousPassword=admin')" != "204" ]]; 
 do sleep 5; 
 done
 
+echo "Password changed successful!!" >> /var/log/temp_sonar.log
+
 #Firewall off
 iptables -D INPUT -p tcp -s localhost --dport 9000 -j ACCEPT
 iptables -D INPUT -p tcp --dport 9000 -j DROP
+
+echo "Firewall OFF" >> /var/log/temp_sonar.log
 
 sed -i "s!@reboot root sleep 30 && ${HOME}/post_install_sonar.sh 2>&1!!" /etc/crontab
 
