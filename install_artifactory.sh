3808b6891190586f027ed61a5a63e931167a0bb3
Sasha <sasha7692@gmail.com>
2020-05-06 20:28:26 +0300

    Update install_artifactory.sh

--- install_artifactory.sh
+++ install_artifactory.sh
@@ -2,8 +2,10 @@
 
 # Install Artifactory on Centos 7
 
-# Turn on logging
-set -x
+set -xe
+
+# Variables
+admin_password=$1
 
 # Check Java
 if ! rpm -qa | grep java; then
@@ -12,12 +14,6 @@ if ! rpm -qa | grep java; then
 else 
 	echo "Java is already installed"
 fi
-
-# Set JAVA_HOME
-echo "export JAVA_HOME=$(dirname $(dirname $(readlink $(readlink $(which javac)))))" >> /etc/profile
-echo "export PATH=$PATH:$JAVA_HOME/bin" >> /etc/profile
-echo "export CLASSPATH=.:$JAVA_HOME/jre/lib:$JAVA_HOME/lib:$JAVA_HOME/lib/tools.jar" >> /etc/profile
-source /etc/profile
 
 # Add Artifactory RPM repository
 curl -sL https://bintray.com/jfrog/artifactory-rpms/rpm | tee /etc/yum.repos.d/bintray-jfrog-artifactory-rpms.repo
@@ -36,6 +32,4 @@ systemctl start artifactory
 systemctl status artifactory
 
 # Set admin password
-admin_password=$1
 curl -XPATCH -uadmin:password http://localhost:8081/access/api/v1/users/admin -H "Content-Type: application/json" -d '{ "password": "'"$admin_password"'" }'
-
